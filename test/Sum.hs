f28a2725103e325c44fcf9723ccc600488780e37
James Haver <mchaver@gmail.com>
2017-10-19 15:21:01 +0800

    Reorganize test code

---
+++ test/Sum.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Sum (spec) where
+
+import Data.Proxy
+import GHC.Generics
+import OCaml.Export
+import Test.Hspec
+import Util
+
+testSum = testOCamlType Sum
+
+spec :: Spec
+spec = do
+  describe "Sum Types" $ do
+    testSum onOrOff "OnOrOff"
+    testSum nameOrIdNumber "NameOrIdNumber"
+    testSum sumVariant "SumVariant"
+    testSum withTuple "WithTuple"
+    testSum sumWithRecord "SumWithRecord"
+    
+data OnOrOff = On | Off
+  deriving (Show,Eq,Generic, OCamlType)
+
+data NameOrIdNumber = Name String | IdNumber Int
+  deriving (Show,Eq,Generic, OCamlType)
+
+data SumVariant
+  = HasNothing
+  | HasSingleInt Int
+  | HasSingleTuple (Int,Int)
+  | HasMultipleInts Int Int
+  | HasMultipleTuples (Int,Int) (Int,Int)
+  deriving (Show,Eq,Generic, OCamlType)  
+
+type Tuple
+  = (Int,Int)
+
+data WithTuple = WithTuple Tuple
+  deriving (Show,Eq,Generic, OCamlType)
+
+data SumWithRecord
+  = A1 {a1 :: Int}
+  | B2 {b2 :: String, b3 :: Int}
+  deriving (Show,Eq,Generic, OCamlType)
+
+onOrOff :: OCamlFile
+onOrOff =
+  OCamlFile
+    "OnOrOff"
+    [ toOCamlTypeSource (Proxy :: Proxy OnOrOff)
+    , toOCamlEncoderSource (Proxy :: Proxy OnOrOff)
+    ]    
+
+nameOrIdNumber :: OCamlFile
+nameOrIdNumber =
+  OCamlFile
+    "NameOrIdNumber"
+    [ toOCamlTypeSource (Proxy :: Proxy NameOrIdNumber)
+    , toOCamlEncoderSource (Proxy :: Proxy NameOrIdNumber)
+    ]
+
+sumVariant :: OCamlFile
+sumVariant =
+  OCamlFile
+    "SumVariant"
+    [ toOCamlTypeSource (Proxy :: Proxy SumVariant)
+    , toOCamlEncoderSource (Proxy :: Proxy SumVariant)
+    ]
+
+tuple :: OCamlFile
+tuple =
+  OCamlFile
+    "Tuple"
+    [ toOCamlTypeSource (Proxy :: Proxy Tuple)
+    , toOCamlEncoderSource (Proxy :: Proxy Tuple)
+    ]
+
+withTuple :: OCamlFile
+withTuple =
+  OCamlFile
+    "WithTuple"
+    [ toOCamlTypeSource (Proxy :: Proxy WithTuple)
+    , toOCamlEncoderSource (Proxy :: Proxy WithTuple)
+    ]
+
+sumWithRecord :: OCamlFile
+sumWithRecord =
+  OCamlFile
+    "SumWithRecord"
+    [ toOCamlTypeSource (Proxy :: Proxy SumWithRecord)
+    , toOCamlEncoderSource (Proxy :: Proxy SumWithRecord)
+    ]
---
+++ test/Util.hs
@@ -0,0 +1,30 @@
+module Util where
+
+import Data.Monoid ((<>))
+import qualified Data.Text.IO as T
+import OCaml.Export
+import Test.Hspec
+
+data ADT
+  = Product
+  | Sum
+  | Primitive
+  | Complex
+
+adtToPath :: ADT -> FilePath
+adtToPath Product = "product"
+adtToPath Sum = "sum"
+adtToPath Primitive = "primitive"
+adtToPath Complex = "complex"
+
+testOCamlType :: ADT -> OCamlFile -> FilePath -> SpecWith ()
+testOCamlType adt ocamlFile typeName =
+  it typeName $ do
+    createOCamlFile testPath ocamlFile
+    automated   <- T.readFile (testPath   <> "/" <> typeName <> ".ml")
+    handWritten <- T.readFile (goldenPath <> "/" <> typeName <> ".ml")
+    automated `shouldBe` handWritten
+  where
+    adtPath    = adtToPath adt
+    testPath   = "test/temp/" <> adtPath
+    goldenPath = "test/golden/" <> adtPath

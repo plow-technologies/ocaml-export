0075a3063cfb958e9fcde50c1efe7db3b05beef5
James Haver <mchaver@gmail.com>
2017-12-19 12:21:53 +0800

    Clean up

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # file: test.sh
 
+stack build
+stack test --no-run-tests 
 stack test &
 servant_pid=$!
-sleep 1
+sleep 5
 
-# npm install --prefix test/interface/golden
-# npm run build --prefix test/interface/golden
+npm install --prefix test/interface/golden
+npm run build --prefix test/interface/golden
 npm run test --prefix test/interface/golden
 
 kill -9 $servant_pid

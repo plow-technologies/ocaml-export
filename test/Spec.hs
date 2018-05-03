2c5861635cc9d5324244d6393c500a8b6ff70978
James Haver <mchaver@gmail.com>
2018-05-03 11:20:29 +0800

    Bring back compiler warnings and other tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -29,7 +29,6 @@ import Network.Wai.Handler.Warp
 main :: IO ()
 main = do
   hspec Product.spec
-{-
   hspec Sum.spec
   hspec File.spec
   hspec D.spec
@@ -45,4 +44,3 @@ main = do
   _ <- forkIO $ run 8082 sumPackageApp
   run 8083 File.filePackageApp
 #endif
--}
